538e2c1afab4ed5e4e18ce24b9031f5ea0b53d52
Esakeuda <127740104+Esakeuda@users.noreply.github.com>
2023-08-25 08:28:29 +0300

    Add project files.

---
+++ TehtavaE1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace IkaKysely
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Syötä ikäsi: ");
+                int ikä = int.Parse(Console.ReadLine());
+
+                // Luodaan viesti käyttäen string interpolation -ominaisuutta
+                string viesti = $"{ikä} – näytät ikäistäsi nuoremmalta";
+
+                // Tulostetaan viesti konsoliin
+                Console.WriteLine(viesti);
+            }
+            catch (FormatException)
+            {
+                // Jos syöte ei ole kelvollinen kokonaisluku, tulostetaan virheviesti
+                Console.WriteLine("Virheellinen syöte. Syötä kokonaisluku ikäsi.");
+            }
+        }
+    }
+}
